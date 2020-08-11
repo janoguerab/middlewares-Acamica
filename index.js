832bb6d290dacb1c9de065182df051d25ddea1b0
Alejandro Noguera <janoguerab@unal.edu.co>
2020-08-10 21:35:13 -0500

    Agregados middlewares para POST y GET, y generic Errors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ server.listen(3000, ()=>{
 
 //Middleware Log {Verbo} - {ruta} - {queryStrings} - {body}
 function agregarLog(req, res, next){
-    
+
     console.log(`${req.method} - ${req.path} - ${JSON.stringify(req.query)} - ${JSON.stringify(req.body)}`);
     next();
 }
@@ -34,21 +34,11 @@ function agregarLog(req, res, next){
 
 // })
 
-//Para todas las rutas
-server.use(bodyParser.json());
-server.use(agregarLog);
-
-
-
-// GET - Obtener todos los contactos
-server.get("/demo", (req,res)=>{
-    res.status(200).json(contactos);
-});
-
 // Middleware - Validar Contacto
 function validarContacto(req, res, next) {
-    const { nombre, apellido } = req.body;
-    if (!nombre || !apellido) {
+    const { nombre, apellido, email } = req.body;
+
+    if (!nombre || !apellido || !email) {
         res.status(400)
            .json("Falta información!!!");
     } else {
@@ -56,12 +46,65 @@ function validarContacto(req, res, next) {
     }
 }
 
+// Middleware - Validar Contacto
+function contactoExist(req, res, next) {
+    const { nombre, apellido, email} = req.body;
+    let exist = false;
+    contactos.forEach((contacto)=>{
+        if(contacto.nombre == nombre
+            && contacto.apellido == apellido
+            && contacto.email == email){
+                exist = true;
+            }
+    })
+    if (exist){
+        res.status(409)
+        .json("El contacto ya existe!!!");
+    }else{
+        next();
+    } 
+}
 
+//Middleware QueryString Version > 5
+function queryVersion(req, res, next){
+    if(req.query.version){
+        if(req.query.version > 5){
+            next();
+        }else{
+            res.status(422).json("Version inferior!!!")
+        }
+    }else{
+            res.status(401).json("Falta especificar la version")
+    } 
+}
+
+// Middleware Errores genericos
+function genericErros(err, req, res, next){
+    console.log(err)
+    if(err){
+        res.status(500).json("Se ha producido un error inesperado.");
+        console.log(`Error: ${err}`);
+    }
+}
+
+
+//Para todas las rutas
+server.use(bodyParser.json());
+server.use(agregarLog);
+server.use(genericErros);
+
+// Middlewares to validate Contact
+let validateContact = [validarContacto, contactoExist]
 // POST - Crear un contacto
-server.post("/contact",validarContacto,(req, res)=>{
+server.post("/contact",validateContact,(req, res)=>{
     contactos.push(req.body)
     res.status(200).json({
         'message':'Contacto agregado!!!', 
         contacto:req.body});   
 });
 
+
+// GET - Obtener todos los contactos
+server.get("/demo",queryVersion, (req,res)=>{
+    res.status(200).json(contactos);
+});
